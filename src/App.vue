c23e1350a75e6a05b73db2640a2a536bb9db54c4
MaryamTavana379 <maryam.tavana379@gmail.com>
2023-09-19 22:42:46 +0330

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,22 @@
-
-
 <template>
-  <div class="inputs">
-    <input
-      type="text"
-      placeholder="type your Height"
-      ref="height"
-    />
-    <input
-      type="text"
-      placeholder="type your Weight"
-      ref="weight"
-    />
-    <button @click="calculate">محاسبه</button>
-    <p>{{ text }}</p>
+  <div class="bmi">
+    <div class="bmi__hero">
+      <h1>BMI</h1>
+      <P> calculator </P>
+    </div>
+    <PageView />
   </div>
 </template>
 
 <script>
+import PageView from "./components/PageView.vue";
 export default {
-  data() {
-    return {
-      text: " ",
-    };
-  },
-  methods: {
-    calculate() {
-      const height = this.$refs.height.value
-      const weight = this.$refs.weight.value
-      const bmi = Math.round( weight / ((height * height) / 10000).toFixed(2))
-   
-      
-     
-      if(height == "" || weight == ""){
-        this.text = "Please enter a valid value...!!"
-      }
-      else if(bmi < 18.6) {
-        this.text = "Under weight : " + bmi
-      }
-      else if(bmi >= 18.6 && bmi < 24.9 ) {
-        this.text = "Normal weight : " + bmi
-
-      }
-      else {
-        this.text = "Over weight : " + bmi
-      }
-
-    },
-  },
+  components: { PageView },
 };
 </script>
 
-<style scoped>
-.inputs {
-  display: flex;
-  flex-direction: column;
+<style>
+body {
+  background: white;
 }
 </style>
---
+++ src/components/PageView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="form">
+    <div class="form__img">
+      <img src="../img/1.jpg" alt="boy" />
+    </div>
+    <div class="form__box">
+      <div class="form__box__inputs">
+        <input type="text" placeholder="type your Height..." ref="height" />
+        <input type="text" placeholder="type your Weight..." ref="weight" />
+      </div>
+      <button @click="calculate">محاسبه</button>
+      <p>{{ text }}</p>
+    </div>
+
+    <div class="form__img">
+      <img src="../img/2.jpg" alt="girl" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: " ",
+    };
+  },
+  methods: {
+    calculate() {
+      const height = this.$refs.height.value;
+      const weight = this.$refs.weight.value;
+      const bmi = Math.round(weight / ((height * height) / 10000).toFixed(2));
+
+      if (height == "" || weight == "") {
+        this.text = "Please enter a valid value...!!";
+      } 
+      else if(bmi<11){
+        this.text = "you're dead...!  :)";
+      }
+      else if (bmi>11 && bmi < 18.6) {
+        this.text = "Under weight : " + bmi;
+      } else if (bmi >= 18.6 && bmi < 24.9) {
+        this.text = "Normal weight : " + bmi;
+      } else {
+        this.text = "Over weight : " + bmi;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
