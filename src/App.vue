03df39260cf3630a661cc6da7438c72b4816ce86
MaryamTavana379 <maryam.tavana379@gmail.com>
2023-09-19 14:16:58 +0330

    bmi-js

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="inputs">
+    <input
+      type="text"
+      placeholder="type your Height"
+      ref="height"
+    />
+    <input
+      type="text"
+      placeholder="type your Weight"
+      ref="weight"
+    />
+    <button @click="calculate">محاسبه</button>
+    <p>{{ text }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      text: " ",
+    };
+  },
+  methods: {
+    calculate() {
+      const height = this.$refs.height.value
+      const weight = this.$refs.weight.value
+      const bmi = Math.round( weight / ((height * height) / 10000).toFixed(2))
+   
+      
+     
+      if(height == "" || weight == ""){
+        this.text = "Please enter a valid value...!!"
+      }
+      else if(bmi < 18.6) {
+        this.text = "Under weight : " + bmi
+      }
+      else if(bmi >= 18.6 && bmi < 24.9 ) {
+        this.text = "Normal weight : " + bmi
+
+      }
+      else {
+        this.text = "Over weight : " + bmi
+      }
+
+    },
+  },
+};
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.inputs {
+  display: flex;
+  flex-direction: column;
 }
 </style>
